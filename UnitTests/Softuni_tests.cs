aa2a87106790d55a4dcc811b89a6376034bfc09f
LazChu <Lakata@gmail.com>
2023-01-30 16:14:41 +0200

    UnitTests for the project

---
+++ UnitTests/Softuni_tests.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+namespace NunitWebDriverTests
+{   
+    public class SoftuniTests
+    {
+        [SetUp] 
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_Assert_page_title()
+        {
+            var driver = new ChromeDriver();
+            driver.Url = "https://softuni.bg";
+            string expectedTitle = "Обучение по програмиране - Софтуерен университет";
+
+
+            Assert.That(driver.Title , Is.EqualTo(expectedTitle));
+
+            driver.Quit();  
+        }
+    }
+}
